c6162b40d14b36a037652e7fb4f9ecf5da781517
Alireza Abedi <aabedi79@gmail.com>
2026-02-04 17:04:14 +0330

    WIP

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CompareTwoClassObjects;
+
+var source = new List<Employee>
+{
+    new Employee { Id = 1, Name = "Ali", HireDate = new DateTime(2022,1,1) },
+    new Employee { Id = 2, Name = "Sara", HireDate = new DateTime(2023,1,1) },
+    new Employee { Id = 3, Name = "hasan", HireDate = new DateTime(2023,1,1) },
+};
+
+var destination = new List<Employee>
+{
+    new Employee { Id = 1, Name = "Ali", HireDate = new DateTime(2023,1,1) }
+};
+
+var result = ReflectionObject.CompareByPropertyValue(source, destination, "Name");
+
+var NewItem = result[CompareResult.NewItem];
+var UpdatedItem = result[CompareResult.UpdatedItem];
+
+Console.Read();
+
---
+++ ReflectionObject.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareTwoClassObjects
+{
+    public enum CompareResult
+    {
+        NewItem,
+        UpdatedItem
+    }
+
+    public static class ReflectionObject
+    {
+
+    public static Dictionary<CompareResult, List<T>> CompareByPropertyValue<T>(
+        List<T> source,
+        List<T> destination,
+        string propertyName
+    ) where T : class
+    {
+        var result = new Dictionary<CompareResult, List<T>>
+        {
+            [CompareResult.NewItem] = new List<T>(),
+            [CompareResult.UpdatedItem] = new List<T>()
+        };
+
+        var property = typeof(T).GetProperty(
+            propertyName,
+            BindingFlags.Public | BindingFlags.Instance
+        );
+
+        if (property == null)
+            throw new ArgumentException(
+                $"Property '{propertyName}' not found on type {typeof(T).Name}");
+
+            var properties = typeof(T)
+            .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+            .Where(p => p.CanRead)
+            .ToArray();
+
+            foreach (var src in source)
+            {
+                var srcValue = property.GetValue(src);
+
+                bool existsInDestination = destination.Any(dest =>
+                    Equals(property.GetValue(dest), srcValue));
+
+                    if (!existsInDestination)
+                    {
+                        result[CompareResult.NewItem].Add(src);
+                    }
+                    else
+                    {
+                        bool exists = destination.Any(dest =>
+                            PropertiesEqual(src, dest, properties));
+
+                        if (!exists)
+                            result[CompareResult.UpdatedItem].Add(src);
+                    }
+            }
+
+            return result;
+        }
+        private static bool PropertiesEqual<T>(T a, T b, PropertyInfo[] properties)
+        {
+            foreach (var prop in properties)
+            {
+                if (!Equals(prop.GetValue(a), prop.GetValue(b)))
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
